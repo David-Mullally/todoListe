ffbd09cb0dc4449279e1f57cdfafaa70e96cd2e1
Dave Mullally <mullallydavid7@gmail.com>
2023-04-14 16:38:49 +0200

    code iteration 2: - save table to local storage

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
-var table = document.getElementById('table');
-var ticketnummer = document.getElementById('ticketnummer');
-var thema = document.getElementById('thema');
-var zusammenfassung = document.getElementById('zusammenfassung');
+var table = document.getElementById("table");
+var ticketnummer = document.getElementById("ticketnummer");
+var thema = document.getElementById("thema");
+var zusammenfassung = document.getElementById("zusammenfassung");
 
 var currentTable = "allgemein";
+var tableContents = [];
+var tableFromLocalStorage = localStorage.getItem("table");
 
-var tableContents = [
-];
+var tableForLocalStorage = tableFromLocalStorage;
+
+table.innerHTML += tableForLocalStorage;
 
 submitForm = () => {
-    let ticket = ticketnummer.value;
-    let the = thema.value;
-    let zf = zusammenfassung.value;
-    tableContents.push(
-        {
-            id: ticket,
-            thema: the,
-            zusammenfassung: zf
-        }
-    );
-    table.innerHTML += `<tr><td>${tableContents[tableContents.length - 1].id}</td><td>${tableContents[tableContents.length - 1].thema}</td><td>${tableContents[tableContents.length - 1].zusammenfassung}</td></tr>`
-    
-    ticketnummer.value = "";
-    thema.value = "";
-    zusammenfassung.value= "";
-};
+  let ticketVal = ticketnummer.value;
+  let themaVal = thema.value;
+  let zfVal = zusammenfassung.value;
 
+  tableContents.push({
+    id: ticketVal,
+    thema: themaVal,
+    zusammenfassung: zfVal,
+  });
+
+  if (tableContents.length >= 1) {
+    tableForLocalStorage += `<tr><td>${
+      tableContents[tableContents.length - 1].id
+    }</td><td>${tableContents[tableContents.length - 1].thema}</td><td>${
+      tableContents[tableContents.length - 1].zusammenfassung
+    }</td></tr>`;
+
+    table.innerHTML += `<tr><td>${
+      tableContents[tableContents.length - 1].id
+    }</td><td>${tableContents[tableContents.length - 1].thema}</td><td>${
+      tableContents[tableContents.length - 1].zusammenfassung
+    }</td></tr>`;
+  }
+  ticketnummer.value = "";
+  thema.value = "";
+  zusammenfassung.value = "";
+  localStorage.setItem("table", tableForLocalStorage);
+};
